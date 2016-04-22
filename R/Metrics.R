17d982050c6f52a5e4e72e11e8bb4a6c030804d6
schuemie <schuemie@ohdsi.org>
2016-04-22 02:37:31 -0400

    Added some more metrics and plots.

---
+++ R/Metrics.R
@@ -0,0 +1,174 @@
+# @file Metrics.R
+#
+# Copyright 2016 Observational Health Data Sciences and Informatics
+#
+# This file is part of MethodEvaluation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Compute the area under the ROC curve
+#'
+#' @export
+computeAuc <- function(methodResults, referenceSet, confidenceIntervals = TRUE) {
+  # TODO: add stratification (e.g. by analysisId, outcomeConceptId)
+  methodResults <- merge(methodResults,
+                         referenceSet[,
+                                      c("exposureConceptId", "outcomeConceptId", "groundTruth")])
+  roc <- pROC::roc.default(methodResults$groundTruth, methodResults$logRr, algorithm = 3)
+  if (confidenceIntervals) {
+    auc <- as.numeric(pROC::ci.auc.roc(roc, method = "delong"))
+    return(data.frame(auc = auc[2], auc_lb95ci = auc[1], auc_lb95ci = auc[3]))
+  } else {
+    auc <- pROC::auc.roc(roc)
+    return(auc[1])
+  }
+}
+
+#' Compute the AUCs for various injected signal sizes
+#'
+#' @param logRr       A vector containing the log of the relative risk as estimated by a method.
+#' @param trueLogRr   A vector containing the injected log(relative risk) for each estimate.
+#' 
+#' @return
+#' A data frame with per injected signal size the AUC and the 95 percent confidence interval of the AUC.
+#' 
+#' @export
+computeAucs <- function(logRr, trueLogRr) {
+  if (any(is.na(logRr))) {
+    warning("Some estimates are NA, removing prior to computing AUCs")
+    trueLogRr <- trueLogRr[!is.na(logRr)]
+    logRr <- logRr[!is.na(logRr)]
+  }
+  trueLogRrLevels <- unique(trueLogRr)
+  if (all(trueLogRrLevels != 0))
+    stop("Requiring at least one true relative risk of 1")
+  result <- data.frame(trueLogRr = trueLogRrLevels[trueLogRrLevels != 0], 
+                       auc = 0, 
+                       aucLb95Ci = 0, 
+                       aucUb95Ci = 0)
+  for (i in 1:nrow(result)) {
+    data <- data.frame(logRr = logRr[trueLogRr == 0 | trueLogRr == result$trueLogRr[i]], 
+                       trueLogRr = trueLogRr[trueLogRr == 0 | trueLogRr == result$trueLogRr[i]])
+    data$truth <- data$trueLogRr != 0
+    roc <- pROC::roc(data$truth, data$logRr, algorithm = 3)
+    auc <- pROC::ci.auc(roc, method = "delong")
+    result$auc[i] <- auc[1]
+    result$aucLb95Ci[i] <- auc[2]
+    result$aucUb95Ci[i] <- auc[3]
+  }
+  return(result)
+}
+
+#' Compute the coverage
+#'
+#' @details
+#' Compute the fractions of estimates where the true effect size is below, above or within the confidence
+#' interval, for one or more true effect sizes.
+#'
+#' @param logRr       A numeric vector of effect estimates on the log scale
+#' @param seLogRr     The standard error of the log of the effect estimates. Hint: often the standard
+#'                    error = (log(<lower bound 95 percent confidence interval>) - log(<effect
+#'                    estimate>))/qnorm(0.025)
+#' @param trueLogRr   A vector of the true effect sizes
+#' @param region      Size of the confidence interval. Default is .95 (95 percent).
+#'
+#' @export
+computeCoverage <- function(logRr, seLogRr, trueLogRr, region = 0.95) {
+  data <- data.frame(logRr = logRr,
+                     logLb95Rr = logRr + qnorm((1 - region)/2) * seLogRr,
+                     logUb95Rr = logRr + qnorm(1 - (1 - region)/2) * seLogRr,
+                     trueLogRr = trueLogRr)
+  if (any(is.na(data$logRr))) {
+    warning("Some estimates are NA, removing prior to computing coverage")
+    data <- data[!is.na(data$logRr), ]
+  }
+  data$aboveCi <- (data$trueLogRr > data$logUb95Rr)
+  data$withCi <- (data$trueLogRr >= data$logLb95Rr & data$logRr <= data$logUb95Rr)
+  data$belowCi <- (data$trueLogRr < data$logLb95Rr)
+  result <- aggregate(aboveCi ~ trueLogRr, data = data, mean)
+  result <- merge(result, aggregate(withCi ~ trueLogRr, data = data, mean))
+  result <- merge(result, aggregate(belowCi ~ trueLogRr, data = data, mean))
+  return(result)
+}
+
+#' Compute the mean squared error
+#'
+#' @export
+computeMse <- function(logRr, trueLogRr) {
+  if (any(is.na(logRr))) {
+    warning("Some estimates are NA, removing prior to computing coverage")
+    trueLogRr <- trueLogRr[!is.na(logRr)]
+    logRr <- logRr[!is.na(logRr)]
+  }
+
+  trueLogRrLevels <- unique(trueLogRr)
+  result <- data.frame(trueLogRr = trueLogRrLevels, 
+                       mse = NA)
+  for (i in 1:nrow(result)) {
+    target <- result$trueLogRr[i]
+    result$mse[i] <- mean((logRr[trueLogRr == target] - target) ^ 2)
+  }
+  return(result)
+}
+
+#' Compute type 1 and 2 error
+#'
+#' @export
+computeType1And2Error <- function(logRr, seLogRr, trueLogRr, alpha = 0.05) {
+  if (any(is.na(logRr))) {
+    warning("Some estimates are NA, removing prior to computing coverage")
+    trueLogRr <- trueLogRr[!is.na(logRr)]
+    logRr <- logRr[!is.na(logRr)]
+  }
+  
+  trueLogRrLevels <- unique(trueLogRr)
+  result <- data.frame(trueLogRr = trueLogRrLevels, 
+                       type1Error = NA,
+                       type2Error = NA)
+  for (i in 1:nrow(result)) {
+    target <- result$trueLogRr[i]
+    p <- EmpiricalCalibration::computeTraditionalP(logRr[trueLogRr == target], seLogRr[trueLogRr == target])
+    if (target == 0){
+        result$type1Error[i] <- mean(p < alpha)
+    } else {
+      result$type2Error[i] <- mean(p > alpha)
+    }
+  }
+  return(result)
+}
+
+#' Compute the AUC, coverage, MSE, and type 1 and 2 error
+#'
+#' @details
+#' Compute the AUC, coverage, MSE, and type 1 and 2 error.
+#'
+#' @param logRr       A numeric vector of effect estimates on the log scale
+#' @param seLogRr     The standard error of the log of the effect estimates. Hint: often the standard
+#'                    error = (log(<lower bound 95 percent confidence interval>) - log(<effect
+#'                    estimate>))/qnorm(0.025)
+#' @param trueLogRr   A vector of the true effect sizes
+#'
+#' @export
+computeMetrics <- function(logRr, seLogRr, trueLogRr) {
+  # data <- EmpiricalCalibration::simulateControls(n = 50 * 3, mean = 0.25, sd = 0.25, trueLogRr = log(c(1, 2, 4))); logRr <- data$logRr; seLogRr <- data$seLogRr; trueLogRr <- data$trueLogRr
+  aucs <- computeAucs(logRr = logRr, trueLogRr = trueLogRr)
+  coverage <- computeCoverage(logRr = logRr, seLogRr = seLogRr, trueLogRr = trueLogRr)
+  coverage <- data.frame(trueLogRr = coverage$trueLogRr,
+                         coverage = coverage$withCi)
+  mse <- computeMse(logRr = logRr, trueLogRr = trueLogRr)
+  type1And2Error <- computeType1And2Error(logRr = logRr, seLogRr = seLogRr, trueLogRr = trueLogRr)
+  metrics <- merge(aucs, coverage, all.y = TRUE)
+  metrics <- merge(metrics, mse)
+  metrics <- merge(metrics, type1And2Error)
+  return(metrics)
+}
---
+++ R/Plots.R
@@ -0,0 +1,144 @@
+# @file Plots.R
+#
+# Copyright 2016 Observational Health Data Sciences and Informatics
+#
+# This file is part of MethodEvaluation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Plot the ROC curves for various injected signal sizes
+#'
+#' @param logRr       A vector containing the log of the relative risk as estimated by a method.
+#' @param trueLogRr   A vector containing the injected log(relative risk) for each estimate.
+#' @param showAucs    Should the AUCs be shown in the plot?
+#' @param fileName    Name of the file where the plot should be saved, for example 'plot.png'. See the
+#'                    function \code{ggsave} in the ggplot2 package for supported file formats.
+#' 
+#' @return
+#' A Ggplot object. Use the \code{ggsave} function to save to file.
+#' 
+#' @export
+plotRocsInjectedSignals <- function(logRr, trueLogRr, showAucs, fileName = NULL) {
+  if (any(is.na(logRr))) {
+    warning("Some estimates are NA, removing prior to computing AUCs")
+    trueLogRr <- trueLogRr[!is.na(logRr)]
+    logRr <- logRr[!is.na(logRr)]
+  }
+  trueLogRrLevels <- unique(trueLogRr)
+  if (all(trueLogRrLevels != 0))
+    stop("Requiring at least one true relative risk of 1")
+  
+  allData <- data.frame()
+  aucs <- c()
+  trueRrs <- c()
+  for (trueLogRrLevel in trueLogRrLevels){
+    if (trueLogRrLevel != 0 ) {
+      # trueLogRrLevel <- log(2)
+      data <- data.frame(logRr = logRr[trueLogRr == 0 | trueLogRr == trueLogRrLevel], 
+                         trueLogRr = trueLogRr[trueLogRr == 0 | trueLogRr == trueLogRrLevel])
+      data$truth <- data$trueLogRr != 0
+      
+      roc <- pROC::roc(data$truth, data$logRr, algorithm = 3)
+      if (showAucs) {
+        aucs <- c(aucs, pROC::auc(roc))
+        trueRrs <- c(trueRrs, exp(trueLogRrLevel))
+      }
+      data <- data.frame(sens = roc$sensitivities, fpRate = 1 - roc$specificities, trueRr = exp(trueLogRrLevel))
+      data <- data[order(data$sens, data$fpRate), ]
+      allData <- rbind(allData, data)
+    }
+  }
+  allData$trueRr <- as.factor(allData$trueRr)
+  plot <- ggplot2::ggplot(allData, ggplot2::aes(x = fpRate, y = sens, group = trueRr, color = trueRr, fill = trueRr)) +
+    ggplot2::geom_abline(intercept = 0, slope = 1) +
+    ggplot2::geom_line(alpha = 0.5, size = 1) +
+    ggplot2::scale_x_continuous("1 - specificity") +
+    ggplot2::scale_y_continuous("Sensitivity")
+  
+  if (showAucs) {
+    aucs <- data.frame(auc = aucs, trueRr = trueRrs) 
+    aucs <- aucs[order(-aucs$trueRr), ]
+    for (i in 1:nrow(aucs)) {
+      label <- paste0("True RR = ",format(round(aucs$trueRr[i], 2), nsmall = 2), ": AUC = ", format(round(aucs$auc[i], 2), nsmall = 2))
+      plot <- plot + ggplot2::geom_text(label = label, x = 1, y = (i-1)*0.1, hjust = 1, color = "#000000")
+    }
+  }
+  if (!is.null(fileName))
+    ggplot2::ggsave(fileName, plot, width = 5.5, height = 4.5, dpi = 400)
+  return(plot)
+}
+
+#' Plot the coverage
+#'
+#' @details
+#' Plot the fractions of estimates where the true effect size is below, above or within the confidence
+#' interval, for one or more true effect sizes.
+#'
+#' @param logRr       A numeric vector of effect estimates on the log scale
+#' @param seLogRr     The standard error of the log of the effect estimates. Hint: often the standard
+#'                    error = (log(<lower bound 95 percent confidence interval>) - log(<effect
+#'                    estimate>))/qnorm(0.025)
+#' @param trueLogRr   A vector of the true effect sizes
+#' @param region      Size of the confidence interval. Default is .95 (95 percent).
+#' @param fileName    Name of the file where the plot should be saved, for example 'plot.png'. See the
+#'                    function \code{ggsave} in the ggplot2 package for supported file formats.
+#'
+#' @examples
+#' data <- simulateControls(n = 50 * 3, mean = 0, sd = 0.15, trueLogRr = log(c(1, 2, 4)))
+#' plotCoverage(data$logRr, data$seLogRr, data$trueLogRr)
+#'
+#' @export
+plotCoverageInjectedSignals  <- function(logRr, seLogRr, trueLogRr, region = 0.95, fileName = NULL) {
+  data <- data.frame(logRr = logRr,
+                     logLb95Rr = logRr + qnorm((1 - region)/2) * seLogRr,
+                     logUb95Rr = logRr + qnorm(1 - (1 - region)/2) * seLogRr,
+                     trueLogRr = trueLogRr,
+                     trueRr = round(exp(trueLogRr), 2))
+  if (any(is.na(data$logRr))) {
+    warning("Some estimates are NA, removing prior to computing coverage")
+    data <- data[!is.na(data$logRr), ]
+  }
+  vizD <- data.frame()
+  for (trueRr in unique(data$trueRr)) {
+    subset <- data[data$trueRr == trueRr, ]
+    d <- data.frame(trueRr = trueRr, group = c("Below CI",
+                                               "Within CI",
+                                               "Above CI"), fraction = 0, pos = 0)
+    d$fraction[1] <- mean(subset$trueLogRr < subset$logLb95Rr)
+    d$fraction[2] <- mean(subset$trueLogRr >= subset$logLb95Rr & subset$trueLogRr <= subset$logUb95Rr)
+    d$fraction[3] <- mean(subset$trueLogRr > subset$logUb95Rr)
+    d$pos[1] <- d$fraction[1]/2
+    d$pos[2] <- d$fraction[1] + (d$fraction[2]/2)
+    d$pos[3] <- d$fraction[1] + d$fraction[2] + (d$fraction[3]/2)
+    vizD <- rbind(vizD, d)
+  }
+  vizD$pos <- sapply(vizD$pos, function(x) {
+    min(max(x, 0.05), 0.95)
+  })
+  
+  vizD$label <- paste(round(100 * vizD$fraction), "%", sep = "")
+  vizD$group <- factor(vizD$group, levels = c("Below CI", "Within CI", "Above CI"))
+  theme <- ggplot2::element_text(colour = "#000000", size = 10)
+  plot <- with(vizD, {
+    ggplot2::ggplot(vizD, ggplot2::aes(x = as.factor(trueRr),
+                                       y = fraction)) + ggplot2::geom_bar(ggplot2::aes(fill = group),
+                                                                          stat = "identity",
+                                                                          position = "stack",
+                                                                          alpha = 0.8) + ggplot2::scale_fill_manual(values = c("#174a9f",
+                                                                                                                               "#f9dd75",
+                                                                                                                               "#f15222")) + ggplot2::geom_text(ggplot2::aes(label = label, y = pos), size = 3) + ggplot2::scale_x_discrete("True relative risk") + ggplot2::scale_y_continuous("Coverage") + ggplot2::theme(panel.grid.minor = ggplot2::element_blank(), panel.background = ggplot2::element_rect(fill = "#FAFAFA", colour = NA), panel.grid.major = ggplot2::element_blank(), axis.ticks = ggplot2::element_blank(), axis.text.y = ggplot2::element_blank(), axis.text.x = theme, legend.key = ggplot2::element_blank(), legend.position = "right")
+  })
+  if (!is.null(fileName))
+    ggplot2::ggsave(fileName, plot, width = 5, height = 3.5, dpi = 400)
+  return(plot)
+}
