8a73317790b1ed44d7969a4700171807feddd70e
schuemie <schuemie@ohdsi.org>
2018-01-18 03:09:40 -0500

    Updating copyright year

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -1,6 +1,6 @@
 # @file PackageMaintenance.R
 #
-# Copyright 2017 Observational Health Data Sciences and Informatics
+# Copyright 2018 Observational Health Data Sciences and Informatics
 #
 # This file is part of MethodEvaluation
 # 
