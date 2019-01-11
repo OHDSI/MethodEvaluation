6781720778e3eeef62f03f485d5cc601ead112c9
Admin_mschuemi <Admin_mschuemi@its.jnj.com>
2019-01-11 03:06:06 -0500

    Fixed bug in positive control synthesis causing only most predictive covariates to be used when predicting outcome probabilities.

--- R/SignalInjection.R
+++ R/SignalInjection.R
@@ -1029,7 +1029,7 @@ injectSurvival <- function(exposures, effectSize, precision, addIntentToTreat) {
   if (nrow(betas) == 1) {
     betas <- data.frame()
   } else {
-    betas <- betas[2:length(betas), ]
+    betas <- betas[2:nrow(betas), ]
     colnames(betas)[colnames(betas) == "id"] <- "covariateId"
   }
   if (nrow(betas) == 0) {
